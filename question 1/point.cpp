6959eb37fa0a3a557bee703c7919ac8d0d9009c5
Vinay Padma <vinay.padma@outlook.com>
2016-06-11 02:29:34 -0400

    final changes before sleep

--- question 1/point.cpp
+++ question 1/point.cpp
@@ -6,3 +6,21 @@ Point::Point(int x, int y, Point& parent):
 x(x),
 y(y),
 parent(parent) {}
+
+Point::~Point()
+{}
+
+const int Point::get_x()
+{
+  return x;
+}
+
+const int Point::get_y()
+{
+  return y;
+}
+
+Point& Point::get_parent()
+{
+  return parent;
+}
